f0d10300c3c3825581c79659094a707932829f61
kdrmmi <103150743+elliwinehouse@users.noreply.github.com>
2022-12-16 12:50:28 -0500

    дешифровать шифр цезаря (английская строка)

---
+++ caesars_cipher_random.py
@@ -0,0 +1,14 @@
+s = input('Введи зашифрованную строку: ')
+d = 'abcdefghijklmnopqrstuvwxyz'
+
+for k in range(25):
+    b = ''
+    for i in range(len(s)):
+        if not s[i].isalpha():
+            b += s[i]
+        else:
+            if s[i].isupper():
+                b += (d[d.find(s[i].lower())-k]).upper()
+            else:
+                b += d[d.find(s[i].lower())-k]
+    print(b)
